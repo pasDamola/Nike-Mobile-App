6904a49f44de34af59b7cdefe84f9313d98143af
Oyindamola Oyetunmibi <oyindadavid18@gmail.com>
2023-02-15 15:38:56 +0100

    added the ProductDetails and ShoppingCart screens

---
+++ src/screens/ProductDetailsScreen.js
@@ -0,0 +1,91 @@
+import {
+  StyleSheet,
+  View,
+  Image,
+  ScrollView,
+  useWindowDimensions,
+} from "react-native";
+import products from "../data/products";
+import { Ionicons } from "@expo/vector-icons";
+
+const ProductDetailsScreen = () => {
+  const width = useWindowDimensions();
+
+  return (
+    <View>
+      <ScrollView>
+        <FlatList
+          data={product.images}
+          renderItem={({ item }) => (
+            <Image source={{ uri: item }} style={{ width, aspectRatio: 1 }} />
+          )}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          pagingEnabled
+        />
+
+        <View style={{ padding: 20 }}>
+          {/* Title */}
+          <Text style={styles.title}>{product.name}</Text>
+
+          {/* Price */}
+          <Text style={styles.price}>${product.price}</Text>
+
+          {/* Description */}
+          <Text style={styles.description}>{product.description}</Text>
+        </View>
+
+        {/* Add to cart button */}
+        <Pressable style={styles.button} onPress={addToCart}>
+          <Text style={styles.buttonText}>Add to cart</Text>
+        </Pressable>
+
+        {/* Navigation icon */}
+      </ScrollView>
+      {/* Image Carousel */}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 34,
+    fontWeight: "500",
+    marginVertical: 10,
+  },
+  price: {
+    fontWeight: "500",
+    fontSize: 16,
+  },
+  description: {
+    marginVertical: 10,
+    fontSize: 18,
+    lineHeight: 30,
+    fontWeight: "300",
+  },
+  button: {
+    backgroundColor: "black",
+    position: "absolute",
+    bottom: 30,
+    width: "90%",
+    alignSelf: "center",
+    alignItems: "center",
+    padding: 20,
+    borderRadius: 100,
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "500",
+    fontSize: 16,
+  },
+  icon: {
+    position: "absolute",
+    top: 50,
+    right: 20,
+    backgroundColor: "#000000AA",
+    borderRadius: 50,
+    padding: 5,
+  },
+});
+
+export default ProductDetailsScreen;
---
+++ src/screens/ShoppingCart.js
@@ -0,0 +1,82 @@
+import { FlatList, StyleSheet } from "react-native";
+import cart from "../data/cart";
+import CartListItem from "../components/CartListItem";
+
+const ShoppingCartTotals = () => {
+  <View style={styles.totalsContainer}>
+    <View style={styles.row}>
+      <Text style={styles.text}>Subtotal</Text>
+      <Text style={styles.text}>410,00 US$</Text>
+    </View>
+    <View style={styles.row}>
+      <Text style={styles.text}>Delivery</Text>
+      <Text style={styles.text}>16,50 US$</Text>
+    </View>
+    <View style={styles.row}>
+      <Text style={styles.textBold}>Total</Text>
+      <Text style={styles.textBold}>426,50 US$</Text>
+    </View>
+  </View>;
+};
+
+const ShoppingCart = () => {
+  return (
+    <>
+      <FlatList
+        data={cart}
+        renderItem={({ item }) => <CartListItem cartItem={item} />}
+        ListFooterComponent={ShoppingCartTotals}
+      />
+      <View style={styles.footer}>
+        <Pressable style={styles.button}>
+          <Text style={styles.buttonText}>Checkout</Text>
+        </Pressable>
+      </View>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  totalsContainer: {
+    margin: 20,
+    paddingTop: 10,
+    borderColor: "gainsboro",
+    borderTopWidth: 1,
+  },
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginVertical: 2,
+  },
+  text: {
+    fontSize: 16,
+    color: "gray",
+  },
+  textBold: {
+    fontSize: 16,
+    fontWeight: "500",
+  },
+  footer: {
+    position: "absolute",
+    bottom: 0,
+    width: "100%",
+    backgroundColor: "white",
+    borderColor: "gainsboro",
+    borderTopWidth: 1,
+    padding: 20,
+  },
+
+  button: {
+    width: "100%",
+    backgroundColor: "black",
+    alignSelf: "center",
+    alignItems: "center",
+    padding: 20,
+    borderRadius: 100,
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "500",
+    fontSize: 16,
+  },
+});
