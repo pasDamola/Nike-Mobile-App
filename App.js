78fff70c8756dd25b924134b64936c5965c60ea9
Oyindamola Oyetunmibi <oyindadavid18@gmail.com>
2023-02-20 23:47:58 +0100

    removed files not being used in the App.js file

--- App.js
+++ App.js
@@ -1,7 +1,4 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
-import ProductDetailsScreen from "./src/screens/ProductDetailsScreen";
-import ShoppingCart from "./src/screens/ShoppingCart";
 import Navigation from "./src/navigation";
 
 export default function App() {
