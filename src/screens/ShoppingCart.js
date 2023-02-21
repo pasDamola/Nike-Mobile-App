123ce44a623c1dcb321b1399a18f5339323057ce
Oyindamola Oyetunmibi <oyindadavid18@gmail.com>
2023-02-21 01:33:44 +0100

    added custom selectors for deliveryFee and total amount

--- src/screens/ShoppingCart.js
+++ src/screens/ShoppingCart.js
@@ -2,23 +2,33 @@ import { StyleSheet, FlatList, Text, View, Pressable } from "react-native";
 import CartListItem from "../components/CartListItem";
 import Button from "../components/Button";
 import { useSelector } from "react-redux";
+import {
+  selectDeliveryPrice,
+  selectSubtotal,
+  selectTotal,
+} from "../store/cartSlice";
 
-const ShoppingCartTotals = () => (
-  <View style={styles.totalsContainer}>
-    <View style={styles.row}>
-      <Text style={styles.text}>Subtotal</Text>
-      <Text style={styles.text}>410,000 US$</Text>
+const ShoppingCartTotals = () => {
+  const subTotal = useSelector(selectSubtotal);
+  const total = useSelector(selectTotal);
+  const deliveryFee = useSelector(selectDeliveryPrice);
+  return (
+    <View style={styles.totalsContainer}>
+      <View style={styles.row}>
+        <Text style={styles.text}>Subtotal</Text>
+        <Text style={styles.text}>{subTotal} US$</Text>
+      </View>
+      <View style={styles.row}>
+        <Text style={styles.text}>Delivery</Text>
+        <Text style={styles.text}>{deliveryFee} US$</Text>
+      </View>
+      <View style={styles.row}>
+        <Text style={styles.textBold}>Total</Text>
+        <Text style={styles.textBold}>{total} US$</Text>
+      </View>
     </View>
-    <View style={styles.row}>
-      <Text style={styles.text}>Delivery</Text>
-      <Text style={styles.text}>410,000 US$</Text>
-    </View>
-    <View style={styles.row}>
-      <Text style={styles.textBold}>Total</Text>
-      <Text style={styles.textBold}>410,000 US$</Text>
-    </View>
-  </View>
-);
+  );
+};
 
 const ShoppingCart = () => {
   const cartItems = useSelector((state) => state.cart.items);
