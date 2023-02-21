449a08d096e0e5584de5dff38fb062e818fc692c
Oyindamola Oyetunmibi <oyindadavid18@gmail.com>
2023-02-21 01:03:32 +0100

    added logic to the add to cart function

--- src/store/cartSlice.js
+++ src/store/cartSlice.js
@@ -12,8 +12,15 @@ export const cartSlice = createSlice({
   reducers: {
     addCartItem: (state, action) => {
       const newProduct = action.payload.product;
+      const cartItem = state.items.find(
+        (item) => item.product.id === newProduct.id
+      );
 
-      state.items.push({ product: newProduct, quantity: 1 });
+      if (cartItem) {
+        cartItem.quantity += 1;
+      } else {
+        state.items.push({ product: newProduct, quantity: 1 });
+      }
     },
     changeQuantity: (state, action) => {},
   },
